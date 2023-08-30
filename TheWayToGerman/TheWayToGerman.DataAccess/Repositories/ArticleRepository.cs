7000348ab7752ca1d8d6ed3be2e3db396704dc53
Mohammed-Al-Mahdi <mohammed-al-mahdi@jaifari.com>
2023-08-30 10:07:21 +0300

    fix build

--- TheWayToGerman/TheWayToGerman.DataAccess/Repositories/ArticleRepository.cs
+++ TheWayToGerman/TheWayToGerman.DataAccess/Repositories/ArticleRepository.cs
@@ -43,7 +43,7 @@ public class ArticleRepository : IArticleRepository
     {
         try
         {
-            return Result.From(PostgresDBContext.Articles.Where(predictate).Select(selector).GetAsyncEnumerator());
+            return Result.From(PostgresDBContext.Articles.Where(predictate).Select(selector));
         }
         catch (Exception ex)
         {
